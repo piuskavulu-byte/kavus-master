2b1b5d15c6c7defc944926095be9b97538e9c87d
piuskavulu-byte <piuskavulu@gmail.com>
2026-04-25 18:30:39 +0300

    Create bot.py

---
+++ src/bot.py
@@ -0,0 +1,7 @@
+cd kavus-master   # the unzipped folder
+git init
+git add .
+git commit -m "Initial commit"
+git branch -M main
+git remote add origin https://github.com/yourusername/kavus-master.git
+git push -u origin main
